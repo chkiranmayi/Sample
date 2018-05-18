589b88c67a27fb8864d1cd66672b5717a3d1344f
chkiranmayi <kiranmayi@headrun.net>
2018-05-18 12:02:14 +0530

    p1.py

--- p1.py
+++
@@ -1,9 +0,0 @@
-num = int(raw_input("Enter number: "))
-if num > 1:
-    for i in range(2,num):
-        if num % 2 == 0 :
-            print("The number is not prime number")
-            break
-    else :
-        print(num,"is a prime number")
-
